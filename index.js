6fd54a91b839ff1212da64b45b64968680579b22
Shaurya Sharma <79473274+ShaSha-Codes@users.noreply.github.com>
2021-08-04 20:48:06 +0530

    Getting Appointment ID Done

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express=require('express');
 const path=require('path');
 const app=express();
+const bodyParser=require('body-parser');
 const patient_model = require("./patient_module");
 const Patient = patient_model.Patient;
+const nanoid=require("nanoid");
+app.use(express.json());
+var urlencodedParser=bodyParser.urlencoded({extended:false});
+
+app.set('view engine','ejs');
 
 
+
+app.post("/patient",urlencodedParser, async (req, res) => {
+	console.log(req.body)
+	let p = await Patient(req.body);
+	console.log(p);
+	let result = p.save();
+	res.render('patient',{id:p.id});
+	
+});
 app.use(express.static(path.join(__dirname,"public"),{extensions:["html"]}));
+
 PORT=process.env.PORT || 5000;
 app.listen(PORT,()=>console.log(`Running on port ${PORT}`));
